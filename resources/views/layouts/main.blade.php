5e231bb63eaddc4b5bc5e42c8da57be2ed540ddd
Miguel Emmara <m.miguelemmara@gmail.com>
2022-05-21 22:25:01 +1200

    Addd scripts sections

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -32,6 +32,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
 
 <body id="page-top" data-bs-spy="scroll" data-bs-target="#mainNav" data-bs-offset="54">
@@ -45,6 +46,8 @@
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
     <script src="assets/js/script.min.js"></script>
+
+    @yield('scripts')
 
 </body>
 
