b27b27edde35c72cf068223f9401f6b35b699a7e
Miguel Emmara <m.miguelemmara@gmail.com>
2022-05-21 16:03:02 +1200

    Modify database rows

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email')->nullable();
+            $table->string('username')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('carsAvailability');
             $table->timestamps();
         });
     }
