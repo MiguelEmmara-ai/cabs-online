e9d8b7163254530a2790d8ecec5d6158f609769f
Miguel Emmara <m.miguelemmara@gmail.com>
2022-05-24 23:34:20 +1200

    Cange to match

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,8 @@ Route::get('/about', function () {
 });
 
 Route::resource('/booking', PassengerController::class);
-Route::post('/continue-booking', [PassengerController::class, 'continueBooking']);
+Route::match(['get', 'post'], '/continue-booking', [PassengerController::class, 'continueBooking']);
+
 Route::get('/cancel-booking', [PassengerController::class, 'cancelBooking']);
 
 Route::get('/register', [RegisterController::class, 'index'])
